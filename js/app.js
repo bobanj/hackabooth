d860c42fc89012cc6d2f0a4ab4db90c7d5e314c2
Ruairi Dorrity <ruairidorrity@gmail.com>
2014-11-06 17:22:31 +0100

    Photos now upload to server. Fixed webcam aspect ratio issue.

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ $(document).ready(function () {
         if (photoUris.length >= 4) {
             inProgress = false;
             toggleCurtains();
-            console.log("ALL PHOTOS TAKEN!! ", photoUris);
+            sendPhotos(photoUris);
             photoUris = [];
         } else {
             console.log("Start COUNTDOWN!!!");
@@ -43,6 +43,20 @@ $(document).ready(function () {
         //countdown.hide();
         takePhoto();
     });
+
+    var sendPhotos = function(photoUris) {
+        $.ajax({
+            url : "http://yelp-mpesce.local:8080/save_images",
+            data : {
+                "image_1" : photoUris[0],
+                "image_2" : photoUris[1],
+                "image_3" : photoUris[2],
+                "image_4" : photoUris[3]
+            },
+            type : "POST",
+            crossDomain: true
+        });
+    }
 
     var takePhoto = function () {
         shutter.load();
@@ -79,13 +93,16 @@ $(document).ready(function () {
     var setCamera = function () {
         Webcam.set({
             // live preview size
-            //width: 800,
-            //height: 600,
+            width: 640,
+            height: 480,
+
+            dest_width: 640,
+            dest_height: 480,
 
 
             // final cropped size
-            crop_width: 640,
-            crop_height: 640,
+            crop_width: 480,
+            crop_height: 480,
 
             // format and quality
             image_format: 'jpeg',
