8c681564211a482639fb9a536fcb575fa75cb927
Ruairi Dorrity <ruairidorrity@gmail.com>
2014-11-06 21:30:42 +0100

    Image expansion in gallery.

--- js/gallery.js
+++ js/gallery.js
@@ -1,16 +1,36 @@
 var currentPhotoUris = [];
 var SERVER_URL = "http://boban.jovanoski.net:8080";
+var imgId = 0;
 
 function showPhotosCollage (photoUris) {
 	for (var i = 0; i < photoUris.length; i++) {
-		var img = $('<img>', { class: 'photo', src: SERVER_URL + photoUris[i] });
+		var img = $('<img>', { id: imgId, class: 'photo', src: SERVER_URL + photoUris[i] });
+		imgId++;
+
+		img.data("uri", SERVER_URL + photoUris[i])
+		if(i === photoUris.length - 1) {
+			img.attr("src", img.data("uri") + "&grid=1")
+		}
+
 		img.css({
 			"box-shadow" : "4px 4px 16px 0px #333333"
 		})
+
 		$('.photos').prepend(img);
 		if(!currentPhotoUris) {
 			currentPhotoUris = []
 		}
+
+		img.on("click", function() {
+			if($(this).attr("src").indexOf("grid") < 0) {
+				$(this).attr("src", $(this).data("uri") + "&grid=1")
+			} else {
+				$(this).attr("src", $(this).data("uri"))
+			}
+			console.log($(this).attr("src"))
+			$('.photos').collagePlus();
+		})
+
 		currentPhotoUris.push(photoUris[i])
 	}
 	$('.photos').collagePlus();
@@ -28,7 +48,6 @@ function showPhotosPile (photoUris) {
 
 		var targetX = x + Math.cos(angle) * 32;
 		var targetY = y + Math.sin(angle) * 32;
-
 
 		img.css({
 			"opacity" : 0,
@@ -64,10 +83,13 @@ function getRecentPhotos() {
 
 function processPhotos(responsePhotoUris) {
 	var newPhotoUris = responsePhotoUris.filter(function(i) {return currentPhotoUris.indexOf(i) < 0;});
-	showPhotosCollage(newPhotoUris)
+	if(newPhotoUris.length > 0) {
+		showPhotosCollage(newPhotoUris)
+	}
 }
 
 $( document ).ready(function() {
 	//setInterval( function() { showPhotos(["http://placehold.it/" + Math.round(Math.random() * 480) + "x" + Math.round(Math.random() * 480)]) } , 2000);
+	getRecentPhotos()
 	setInterval(getRecentPhotos, 5000)
 });
