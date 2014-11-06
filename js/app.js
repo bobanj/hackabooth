bf0e4c6e686af44ac68975411b60119be96f4166
Boban Jovanoski <bobanj@gmail.com>
2014-11-06 13:44:18 +0100

    js behaviour

--- js/app.js
+++ js/app.js
@@ -1,21 +1,53 @@
 $(document).ready(function() {
 
-    $curtainopen = false;
+    var curtainopen = false;
+    var rope = $('.rope');
 
-    $(".rope").click(function(){
-        $(this).blur();
-        if ($curtainopen == false){
+    var setCamera = function(){
+        Webcam.set({
+            // live preview size
+            //width: 800,
+            //height: 600,
+
+
+            // final cropped size
+            crop_width: 640,
+            crop_height: 640,
+
+            // format and quality
+            image_format: 'jpeg',
+            jpeg_quality: 90
+        });
+        Webcam.attach('#camera');
+    };
+
+    var toggleCurtains = function(){
+        if (curtainopen == false){
             $(this).stop().animate({top: '0px' }, {queue:false, duration:350, easing:'easeOutBounce'});
-            $(".leftcurtain").stop().animate({width:'130px'}, 2000 );
-            $(".rightcurtain").stop().animate({width:'130px'},2000 );
-            $curtainopen = true;
+            $(".leftcurtain").stop().animate({width:'240px'}, 2000 );
+            $(".rightcurtain").stop().animate({width:'240px'},2000 );
+            curtainopen = true;
         }else{
             $(this).stop().animate({top: '-40px' }, {queue:false, duration:350, easing:'easeOutBounce'});
             $(".leftcurtain").stop().animate({width:'50%'}, 2000 );
             $(".rightcurtain").stop().animate({width:'51%'}, 2000 );
-            $curtainopen = false;
+            curtainopen = false;
         }
+    };
+
+    rope.click(function(e){
+        e.preventDefault();
+        $(this).blur();
+        toggleCurtains();
         return false;
     });
+
+    $('body').keyup(function(e){
+        if(e.keyCode == 32){
+            rope.click();
+        }
+    });
+
+    setCamera();
 
 });
