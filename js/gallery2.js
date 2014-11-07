d7fc2185ed47a5383b1cdd32a36a759096e971b6
Boban Jovanoski <bobanj@gmail.com>
2014-11-07 02:31:24 +0100

    cleanup, using lightbox, added gallery2

--- js/gallery2.js
+++ js/gallery2.js
@@ -8,14 +8,14 @@ $( document ).ready(function() {
         var gridPhotoUrl = photoUrl + '&grid=1';
         var photoHTML = [
             '<div class="col-lg-3 col-md-4 col-xs-6 thumb">',
-                '<a class="thumbnail" href="', gridPhotoUrl, '">',
-                    '<img class="img-responsive" src="', photoUrl, '" alt="">',
+                '<a class="thumbnail" href="', gridPhotoUrl, '" data-lightbox="image">',
+                    '<img class="img-responsive" src="', gridPhotoUrl, '" alt="">',
                 '</a>',
           '</div>'
         ];
-        row.append($(photoHTML.join('')));
-
+        return photoHTML.join('');
     };
+
     function getRecentPhotos() {
         $.ajax({
             url : SERVER_URL + "/recent_images",
@@ -23,16 +23,49 @@ $( document ).ready(function() {
             crossDomain: true,
             success : processPhotos
         });
-    }
+    };
+
+//    function getRandomInt(min, max) {
+//        return Math.floor(Math.random() * (max - min + 1)) + min;
+//    };
 
     function processPhotos(responsePhotoUris) {
         var newPhotoUris = responsePhotoUris.filter(function(i) {return currentPhotoUris.indexOf(i) < 0;});
         if(newPhotoUris.length > 0) {
             $.each(newPhotoUris, function(i, photoUrl){
-                row.append(generatePhotoHtml(photoUrl));
+                if (i < 3){
+                    var photoHtml = generatePhotoHtml(photoUrl);
+                    row.prepend(photoHtml);
+                    currentPhotoUris.push(photoUrl);
+                }
             });
+            row.find("a:first").click();
         }
     }
+//    var swapThumbnailData = function(link){
+//        var image = link.children("img");
+//        var pom = image.attr("src");
+//
+//        image.attr("src", link.attr("href"));
+//        link.attr("href", pom);
+//    };
+//    $('.container').on('click', '.thumbnail', function(e){
+//        e.preventDefault();
+//        var link = $(this);
+//        if(!link.hasClass("grid")){
+//            swapThumbnailData(link);
+//            link.addClass("grid");
+//        }
+//    });
+//
+//    $('.container').on('click', '.grid', function(e){
+//        e.preventDefault();
+//        var link = $(this);
+//        swapThumbnailData(link);
+//        link.removeClass("grid");
+//    });
 
     getRecentPhotos();
+    //TODO kill setInterval
+    setInterval(getRecentPhotos, 5000);
 });
