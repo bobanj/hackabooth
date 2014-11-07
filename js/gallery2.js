78426f5e0c9b211fbd0317c6210be9c6593564cd
Boban Jovanoski <bobanj@gmail.com>
2014-11-07 18:10:43 +0100

    limiiitt

--- js/gallery2.js
+++ js/gallery2.js
@@ -19,7 +19,7 @@ $( document ).ready(function() {
     function getRecentPhotos() {
         console.log("getting stuff");
         $.ajax({
-            url : SERVER_URL + "/gallery_images?limit=200",
+            url : SERVER_URL + "/gallery_images?limit=2000",
             type : "GET",
             crossDomain: true,
             success : processPhotos,
